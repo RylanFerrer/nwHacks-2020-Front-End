6a2907c1ae08731a59e20479ba3f614c429f6fe2
Stefan Layanto <stef.layanto@gmail.com>
2020-01-12 09:50:00 -0800

    i hope i dont break your repo

--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
 const PORT = 5000 
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
+app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
+app.use(cors())
 let datas = []
-app.all('/', jsonParser,async(req,res) => {
-    if(req.body !== {})
+app.all('/',async(req,res) => {
+    if(req.body.noise)
     {
      datas = req.body;
     }
+    console.log(JSON.stringify(req.body));
     res.json(datas)
 })
 
