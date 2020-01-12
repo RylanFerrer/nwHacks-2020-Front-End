66bdd6cf9f17317c5d0bcebd52fa60b91eacceea
RylanFerrer <rylanferrer@gmail.com>
2020-01-11 21:02:41 -0800

    added break functions

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Chart from './Components/Charts/Chart'
 import logo from './logo.svg';
 import './App.css';
 import "./Styles/main.css"
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <h1>Hackathon </h1>
       <DistractionBreakdown />
+      <Chart/>
     </div>
   );
 }
--- src/Components/Charts/Chart.js
+++ src/Components/Charts/Chart.js
@@ -3,21 +3,46 @@ import CanvasJSReact from '../../Assets/Plugins/canvasjs.react';
 import {options} from './Chart Data/options' 
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
-var dps = [{x: 1, y: 10}];   //dataPoints.
-var xVal = dps.length + 1;
-var yVal = 15;
-var updateInterval = 10000;
-
+let dps = [{x: 1, y: 10}, {x: 2, y: 10},];   //dataPoints.
+let xVal = dps.length + 1;
+let yVal = 15;
+let updateInterval = 10000;
+let option =  options(dps)
 class Chart extends Component {
+	state = {
+		onBreak: false
+	}
 	componentDidMount() {
 		setInterval(this.updateChart, updateInterval);
 	}
+	changeBreak = () => {
+		this.setState({
+			onBreak: !this.state.onBreak
+		}, () => { 
+				option.axisX.scaleBreaks.customBreaks.push(
+					{
+							startValue: xVal,
+							endValue: xVal + 1,
+							color: "black",
+							lineThickness: 1,
+							type: "straight"
+					})
+		})
+	}
 	updateChart  = () =>  {
-		yVal = yVal +  Math.round(5 + Math.random() *(-5-5));
-		dps.push({x: xVal,y: yVal});
-		xVal+= 1;
-		if (dps.length >  10 ) {
-			dps.shift();
+		if (!this.state.onBreak)
+		{
+			//this will be replaced when actual data is received
+			yVal = yVal +  Math.round(5 + Math.random() * (-5-5));
+			dps.push({x: xVal,y: yVal});
+			xVal+= 1;
+			if (dps.length >  10 ) {
+				dps.shift();
+			}
+		} else {
+			yVal = 0
+			dps.push({x: xVal,y: null});
+			xVal+= 1;
 		}
 		this.chart.render();
 	}
@@ -25,9 +50,10 @@ class Chart extends Component {
 		return (
 		<div>
 			<h1>React Dynamic Line Chart</h1>
-			<CanvasJSChart options = {options(dps)} 
+			<CanvasJSChart options = {option} 
 				onRef={ref => this.chart = ref}
 			/>
+			<button onClick = {() => this.changeBreak()}>Change Break</button>
 		</div>
 		);
 	}
